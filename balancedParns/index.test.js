aae1f907e6e02ba20602c0ff74f2e05fda91aad1
afnansdamra <afnan.damra@optimumpartners.co>
2021-09-16 11:34:31 +0300

    bracket validation

--- balancedParns/index.test.js
+++ balancedParns/index.test.js
@@ -25,5 +25,39 @@
  */
 
  function balancedParens(input) {
-  //  TO DO
+  input = input.match(/[()[\]{}]/g);
+  const brackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+  const arr = [];
+  for (let i = 0; i < input.length; i++) {
+    if (Object.keys(brackets).includes(input[i])) {
+      arr.push(input[i]);
+    } else {
+      if (input[i] !== brackets[arr.pop()]) return false;
+    }
+  }
+  if (arr.length !== 0) return false;
+  return true;
 }
+
+describe('Test', () => {
+  it('should return true when called if the brackets in the string are balanced', () => {
+    expect(balancedParens('{}')).toBeTruthy();
+    expect(balancedParens('{}(){}')).toBeTruthy();
+    expect(balancedParens('()[[Extra Characters]]')).toBeTruthy();
+    expect(balancedParens('(){}[[]]')).toBeTruthy();
+    expect(balancedParens('{}{Afnan}[Damra](())')).toBeTruthy();
+    expect(balancedParens(' var wow  = { yo: thisIsAwesome() }')).toBeTruthy();
+  })
+
+  it('should return false when called if the brackets in the string are not balanced', () => {
+    expect(balancedParens(')(')).toBeFalsy();
+    expect(balancedParens('[({}]')).toBeFalsy();
+    expect(balancedParens('(](')).toBeFalsy();
+    expect(balancedParens('{(})')).toBeFalsy();
+    expect(balancedParens(' var hubble = function() { telescopes.awesome();')).toBeFalsy();
+  })
+});
