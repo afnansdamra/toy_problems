82a700ab777b706f64a9671102dd7815316b4f2a
afnansdamra <afnan.damra@optimumpartners.co>
2021-09-26 11:20:44 +0300

    linked list

--- linkedListAdvance/index.test.js
+++ linkedListAdvance/index.test.js
@@ -19,8 +19,70 @@
 // list.tail.value;   //yields '5';
 
 
-const LinkedList = () => {
-  //fill me in!
-};
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-//write methods here!
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToTail(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      const lastNode = this.head;
+      while (lastNode.next)
+        lastNode = lastNode.next;
+      lastNode.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  removeHead() {
+    if (!this.head) {
+      return null
+    } else {
+      const headNode = this.head
+      this.head = headNode.next
+      return headNode.value
+    }
+  }
+
+  contains(value) {
+    if (this.head) {
+      let searchNode = this.head
+      if (searchNode.value === value) return true
+      while (searchNode.next) {
+        searchNode = searchNode.next
+        if (searchNode.value === value) return true
+      }
+    }
+    return false;
+  }
+}
+
+
+
+describe("Tests", () => {
+	it("test LinkedList", () => {
+    const list = new LinkedList();
+    expect(list.tail).toBeNull()
+    list.addToTail(4)
+    list.addToTail(5)
+    expect(list.tail.value).toEqual(5)
+    expect(list.head.value).toEqual(4)
+    expect(list.contains(5)).toBeTruthy()
+    expect(list.contains(6)).toBeFalsy()
+    expect(list.removeHead()).toEqual(4)
+    expect(list.head.value).toEqual(5)
+    expect(list.tail.value).toEqual(5)
+	})
+})
