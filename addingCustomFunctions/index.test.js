74d10c11f369e2955a12b848b26f7a701160ac71
afnansdamra <afnan.damra@optimumpartners.co>
2021-10-05 11:43:40 +0300

    extend

--- addingCustomFunctions/index.test.js
+++ addingCustomFunctions/index.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-extend-native */
 /**
  * adding custom functions into native array
  * 1- sort function
@@ -6,4 +7,57 @@
  * 4- search for value
  */
 
-//  TODO
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        ;[array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+};
+
+Object.defineProperty(Array.prototype, 'newSort', {
+  value: function() {
+    return bubbleSort(this);
+  },
+});
+
+/**
+ * doesn't need parameters, part of the array object and exists in every array
+ * @return the first element in the array
+ */
+Array.prototype.getFirstElement = function() {
+  return this[0];
+};
+
+/**
+ * doesn't need parameters, part of the array object and exists in every array
+ * @return the last element in the array
+ */
+Array.prototype.getLastElement = function() {
+  return this[this.length - 1];
+};
+
+Object.defineProperty(Array.prototype, 'search', {
+  value: function(x) {
+    for (let i = 0; i < this.length; i++) {
+      if (x === this[i]) {
+        return i;
+      }
+    }
+    return -1;
+  },
+});
+
+const arr = [-7, 50, 2, 37, 10, 28];
+describe('extend', () => {
+  it('test', () => {
+    expect(arr.getFirstElement()).toEqual(-7);
+    expect(arr.getLastElement()).toEqual(28);
+    expect(arr.search(37)).toEqual(3);
+    expect(arr.search(38)).toEqual(-1);
+    expect(arr.newSort()).toEqual([-7, 2, 10, 28, 37, 50]);
+  });
+});
