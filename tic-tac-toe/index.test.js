a9cdf2cd6f81c8f5af0f71c03d288730c44b15af
afnansdamra <afnan.damra@optimumpartners.co>
2021-10-10 14:13:17 +0300

    TicTacToe

--- tic-tac-toe/index.test.js
+++ tic-tac-toe/index.test.js
@@ -23,3 +23,82 @@ var solvedBoard = [[0,1,1],
                    [2,1,2]];
 ticTacToe(solvedBoard);//should return 1
 */
+
+const detectTicTacToeWin = (board) => {
+  const possibilites = [];
+
+  board.forEach((row) => possibilites.push(row));
+
+  board.forEach((row, i) => {
+    const columns = board.map((cols, j) => {
+      return board[j][i];
+    });
+    possibilites.push(columns);
+  });
+
+  let leftDiagonal = -1;
+  const dia1 = board.map(() => {
+    leftDiagonal++;
+    return board[leftDiagonal][leftDiagonal];
+  });
+
+  let rightDiagonal = board.length;
+  const dia2 = board.map(() => {
+    leftDiagonal++;
+    rightDiagonal--;
+    return board[leftDiagonal - board.length][rightDiagonal];
+  });
+
+  possibilites.push(dia1, dia2);
+
+  console.log('POSSIBILITES', possibilites); // length = 8
+
+  const result = [];
+  for (let i = 0; i < possibilites.length; i++) {
+    if (new Set(possibilites[i]).size === 1 && !(possibilites[i][0] === '0')) {
+      result.push(possibilites[i]);
+      console.log('RESULT', result);
+    }
+  }
+
+  return (result.length > 0) ? Number(result[0][0]) : -1;
+};
+
+describe('detectTicTacToeWin', () => {
+  test('It should return the winner number if there are three in a row', () => {
+    expect(
+        detectTicTacToeWin([
+          ['1', '0', '2'],
+          ['1', '2', '0'],
+          ['1', '2', '1'],
+        ]),
+    ).toStrictEqual(1);
+    expect(
+        detectTicTacToeWin([
+          ['2', '0', '1'],
+          ['1', '2', '1'],
+          ['1', '0', '2'],
+        ]),
+    ).toStrictEqual(2);
+  });
+
+  test('It should return false if there are not three in a row', () => {
+    expect(
+        detectTicTacToeWin([
+          ['1', '0', '2'],
+          ['2', '2', '0'],
+          ['1', '2', '1'],
+        ]),
+    ).toStrictEqual(-1);
+  });
+
+  test('It should not treat empty 3 in row as winner', () => {
+    expect(
+        detectTicTacToeWin([
+          ['0', '0', '0'],
+          ['2', '2', '0'],
+          ['1', '2', '1'],
+        ]),
+    ).toEqual(-1);
+  });
+});
