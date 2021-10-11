fc934c3c78b1a1f9656184d0463f94e3132d87e2
afnansdamra <afnan.damra@optimumpartners.co>
2021-10-11 11:41:26 +0300

    compose / pipe

--- composePipe/index.test.js
+++ composePipe/index.test.js
@@ -32,3 +32,55 @@
   var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
   addAndMultiplyTwice(5); //should be 63
  */
+
+/**
+ * @param  {...any} args as many functions that you need to compose together
+ * @return {any} the value from all the functions
+ */
+function compose(...args) {
+  return (arg) => {
+    for (let i = args.length - 1; i >= 0; i--) arg = args[i](arg);
+    return arg;
+  };
+}
+
+/**
+ * @param  {...any} args as many functions that you need to pipe together
+ * @return {any} the value from all the functions
+ */
+function pipe(...args) {
+  return (arg) => {
+    for (let i = 0; i < args.length; i++) arg = args[i](arg);
+    return arg;
+  };
+}
+
+describe('compose', () => {
+  const greet = function(name) {
+    return 'hello ' + name;
+  };
+  const exclaim = function(statement) {
+    return statement.toUpperCase() + '!';
+  };
+  const welcome = compose(exclaim, greet);
+  test('It should compose functions', () => {
+    expect(welcome('phillip')).toEqual('HELLO PHILLIP!');
+  });
+});
+
+describe('pipe', () => {
+  const add2 = function(number) {
+    return number + 2;
+  };
+  const multiplyBy3 = function(number) {
+    return number * 3;
+  };
+  const addAndMultiply = pipe(add2, multiplyBy3);
+  test('It should pipe functions', () => {
+    expect(addAndMultiply(5)).toEqual(21);
+  });
+  const addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
+  test('It should pipe functions', () => {
+    expect(addAndMultiplyTwice(5)).toEqual(63);
+  });
+});
