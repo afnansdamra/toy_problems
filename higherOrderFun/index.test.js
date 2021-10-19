422e985e3473aa1b50b9a3c6513a664b4a630f42
afnansdamra <afnan.damra@optimumpartners.co>
2021-10-19 11:46:21 +0300

    HigherOrderFun

--- higherOrderFun/index.test.js
+++ higherOrderFun/index.test.js
@@ -8,8 +8,13 @@ of calling the callback function.
 See example usage to understand what arguments are passed to the callback.
 */
 
+// eslint-disable-next-line no-extend-native
 Array.prototype.map = function(callback) {
-
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
 };
 
 /*
@@ -19,7 +24,6 @@ var transform = function(element,index,array){
 };
 ["a","b","c"].map(transform); //should return ['a0a','b1b','c2c'];
 */
-
 
 /*
 Problem 2:
@@ -32,7 +36,14 @@ Please see example usage to understand what should be passed to the callback.
 */
 
 const asyncSum = function(a, b, callback) {
-
+  if (typeof a === 'number' && typeof b === 'number') {
+    setTimeout(async () => {
+      const c = a + b;
+      return callback('', c);
+    }, 1000);
+  } else {
+    return callback('Incorrect argument(s)');
+  }
 };
 
 /*
@@ -42,8 +53,10 @@ Example use:
 const logNumber = function(error, number) {
   if (error) {
     console.log('Error: ', error);
+    return 'Error: ' + error;
   } else {
     console.log('The total is: ', number);
+    return 'The total is: ' + number;
   }
 };
 
@@ -53,9 +66,26 @@ asyncSum(10,"B",logNumber);
 //should print "Error: Incorrect argument(s)" after 1 second
 */
 
-
 /*
 Problem 3 (ADVANCED):
 What kind of candy do you like?
-Your answer:
+Your answer: ICECREAM
 */
+
+describe('HigherOrderFun', () => {
+  it('map', () => {
+    const transform = function(element, index, array) {
+      return array[index] + index + element;
+    };
+    expect(['a', 'b', 'c'].map(transform)).toEqual(['a0a', 'b1b', 'c2c']);
+  });
+  it('asyncSum', () => {
+    jest.useFakeTimers();
+    setTimeout(() => {
+      expect(asyncSum(10, 7, logNumber)).toEqual('The total is: 17');
+    }, 1000);
+    expect(asyncSum(10, 'B', logNumber)).toEqual(
+        'Error: Incorrect argument(s)',
+    );
+  });
+});
