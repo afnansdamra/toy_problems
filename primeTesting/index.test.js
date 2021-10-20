a6d77ba731c95b94016883329e9de40a7689ca73
afnansdamra <afnan.damra@optimumpartners.co>
2021-10-20 11:51:27 +0300

    primeTesting

--- primeTesting/index.test.js
+++ primeTesting/index.test.js
@@ -6,7 +6,12 @@
  */
 
 const primeTester = function(n) {
-
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
@@ -15,3 +20,24 @@ const primeTester = function(n) {
   * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
   * saucy, check out the Sieve of Atkin.)
 */
+
+const generatePrime = function(start, end) {
+  const primes = [];
+  for (let i = start; i <= end; i++) {
+    if (primeTester(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+};
+
+describe('primeTesting', () => {
+  it('primeTester', () => {
+    expect(primeTester(7)).toBeTruthy();
+    expect(primeTester(8)).toBeFalsy();
+  });
+  it('generatePrime', () => {
+    expect(generatePrime(1, 8)).toEqual([1, 2, 3, 5, 7]);
+    expect(generatePrime(10, 20)).toEqual([11, 13, 17, 19]);
+  });
+});
