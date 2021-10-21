3b6a0fa9fbec428f9fc9891d2e26aaeff92cf672
afnansdamra <afnan.damra@optimumpartners.co>
2021-10-21 11:46:57 +0300

    isSubsetOf

--- subset/index.test.js
+++ subset/index.test.js
@@ -26,5 +26,34 @@ Make the method work for arrays that contain objects and/or arrays as elements.
 
 // eslint-disable-next-line no-extend-native
 Array.prototype.isSubsetOf = function(arr) {
-
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]] = arr[i];
+  }
+  for (let j = 0; j < this.length; j++) {
+    if (!obj[this[j]]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+describe('isSubsetOf', () => {
+  it('test', () => {
+    expect(['2'].isSubsetOf(['2', '3', '4', '5'])).toBeTruthy();
+    const a = ['commit', 'push'];
+    expect(a.isSubsetOf(['commit', 'rebase', 'push', 'blame'])).toBeTruthy();
+    const b = ['merge', 'reset', 'reset'];
+    expect(b.isSubsetOf(['reset', 'merge', 'add', 'commit'])).toBeTruthy();
+    expect(['1'].isSubsetOf(['2', '3', '4', '5'])).toBeFalsy();
+  });
+  it('test', () => {
+    const a = [{commit: 'commit', push: 'push'}, {commit: 'commit'}];
+    expect(a.isSubsetOf([{
+      commit: 'commit',
+      rebase: 'rebase',
+      push: 'push',
+      blame: 'blame',
+    }])).toBeTruthy();
+  });
+});
